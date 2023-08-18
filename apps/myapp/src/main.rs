a6a6b094bc92813a4071d257617bf8fe851b4215
Anastasiia Havriushenko <ahavrius@Anastasiias-MacBook-Pro.local>
2023-08-18 12:53:56 +0200

    add restart screen

--- apps/myapp/src/main.rs
+++ apps/myapp/src/main.rs
@@ -28,6 +28,12 @@ impl Pipe {
 }
 
 
+enum GameMode {
+    Playing,
+    GameOver,
+}
+
+
 struct GameState {
     bird: Bird,
     pipes: Vec<Pipe>,
@@ -54,62 +60,72 @@ impl GameState {
 async fn main() {
     let mut game_state = GameState::new();
     let mut restart_game = false;
+    let mut mode = GameMode::Playing;
 
 
     loop {
-        // Bird physics
-        game_state.bird.velocity += GRAVITY;
-        game_state.bird.position.y += game_state.bird.velocity;
+        match mode {
+            GameMode::Playing => {
+                // Bird physics
+                game_state.bird.velocity += GRAVITY;
+                game_state.bird.position.y += game_state.bird.velocity;
 
-        if is_mouse_button_pressed(MouseButton::Left) {
-            game_state.bird.velocity = JUMP_STRENGTH;
-        }
+                if is_mouse_button_pressed(MouseButton::Left) {
+                    game_state.bird.velocity = JUMP_STRENGTH;
+                }
 
-        // Pipe logic
-        for pipe in &mut game_state.pipes {
-            pipe.position.x += PIPE_SPEED;
-        }
+                // Pipe logic
+                for pipe in &mut game_state.pipes {
+                    pipe.position.x += PIPE_SPEED;
+                }
 
-        if game_state.pipes[0].position.x + 60.0 < 0.0 {
-            game_state.pipes.remove(0);
-            game_state.pipes.push(Pipe::new(SCREEN_WIDTH));
-        }
+                if game_state.pipes[0].position.x + 60.0 < 0.0 {
+                    game_state.pipes.remove(0);
+                    game_state.pipes.push(Pipe::new(SCREEN_WIDTH));
+                }
 
-        // Collision with ground or ceiling
-        if game_state.bird.position.y <= 0.0 || game_state.bird.position.y >= SCREEN_HEIGHT {
-            restart_game = true;
-        }
+                // Collision with ground or ceiling
+                if game_state.bird.position.y <= 0.0 || game_state.bird.position.y >= SCREEN_HEIGHT {
+                    mode = GameMode::GameOver;
+                }
 
-        // Collision with pipes
-        for pipe in &game_state.pipes {
-            if game_state.bird.position.y < pipe.gap_y - 50.0 || game_state.bird.position.y > pipe.gap_y + 50.0 {
-                if game_state.bird.position.x > pipe.position.x && game_state.bird.position.x < pipe.position.x + 60.0 {
-                    restart_game = true;
-                    break; // no need to check further pipes if we're restarting
+                // Collision with pipes
+                for pipe in &game_state.pipes {
+                    if game_state.bird.position.y < pipe.gap_y - 50.0 || game_state.bird.position.y > pipe.gap_y + 50.0 {
+                        if game_state.bird.position.x > pipe.position.x && game_state.bird.position.x < pipe.position.x + 60.0 {
+                            mode = GameMode::GameOver;
+                            break; // no need to check further pipes if we're restarting
+                        }
+                    }
+                }
+                 // ... [drawing code] ...
+
+
+                clear_background(SKYBLUE);
+
+                // Draw bird
+                draw_circle(game_state.bird.position.x, game_state.bird.position.y, 20.0, YELLOW);
+
+                // Draw pipes
+                for pipe in &game_state.pipes {
+                    draw_rectangle(pipe.position.x, 0.0, 60.0, pipe.gap_y - 50.0, DARKGREEN);
+                    draw_rectangle(pipe.position.x, pipe.gap_y + 50.0, 60.0, SCREEN_HEIGHT - pipe.gap_y - 50.0, DARKGREEN);
+                }
+            }
+
+            GameMode::GameOver => {
+                clear_background(SKYBLUE);
+                
+                draw_text("Game Over", SCREEN_WIDTH / 2.0 - 100.0, SCREEN_HEIGHT / 2.0 - 20.0, 40.0, WHITE);
+                draw_text("Click to restart!", SCREEN_WIDTH / 2.0 - 150.0, SCREEN_HEIGHT / 2.0 + 30.0, 30.0, WHITE);
+
+                if is_mouse_button_pressed(MouseButton::Left) {
+                    game_state = GameState::new();
+                    mode = GameMode::Playing;
                 }
             }
         }
-
-        if restart_game {
-            game_state.restart();
-            restart_game = false;
-            continue; // skip drawing and go to the next loop iteration
-        }
-
-        // ... [drawing code] ...
-
-
-        clear_background(SKYBLUE);
-
-        // Draw bird
-        draw_circle(game_state.bird.position.x, game_state.bird.position.y, 20.0, YELLOW);
-
-        // Draw pipes
-        for pipe in &game_state.pipes {
-            draw_rectangle(pipe.position.x, 0.0, 60.0, pipe.gap_y - 50.0, DARKGREEN);
-            draw_rectangle(pipe.position.x, pipe.gap_y + 50.0, 60.0, SCREEN_HEIGHT - pipe.gap_y - 50.0, DARKGREEN);
-        }
-
+       
         next_frame().await;
     }
 }
